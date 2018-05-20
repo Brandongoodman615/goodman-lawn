0fffde4e50dfb73baf3c31ebac57b962081ddff1
brandongoodman615 <brandongoodman615@gmail.com>
2018-05-20 04:14:57 +0000

    Setup the request service form that stores user info in database.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,19 @@
 class JobsController < ApplicationController
 
   def new
+    @job = Job.new
     @jobs = Job.all
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:name, :address, :email)
   end
 
 end
