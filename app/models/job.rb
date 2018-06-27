f40a2768a29f78f981de3c056bbac256b4b85470
brandongoodman615 <brandongoodman615@gmail.com>
2018-06-27 20:42:37 +0000

    added gem geocoder and figaro, setup Jobs to have long and lat and setup google api key.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ApplicationRecord
-
+  geocoded_by :address
+  after_validation :geocode 
 
   validates :name, presence: true, length: {maximum: 30, minimum: 5}
   validates :address, presence: true
